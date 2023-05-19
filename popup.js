8cebb19ccf08bf3265cb895a02f849945b904c3f
Sumit <thesumit0013@gmail.com>
2023-05-19 18:46:19 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+   var header = document.querySelector("h1");
+   header.addEventListener("click", function() {
+      alert("Hello World!");
+   });
+});
